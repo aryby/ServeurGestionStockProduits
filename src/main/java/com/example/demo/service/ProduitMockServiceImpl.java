cbefcd85f6f50badaa1cd1e910bf136b55263daa
AZZEDINE EL ARYBY <aryby00@gmail.com>
2019-02-13 19:29:11 +0100

    crud_produit

---
+++ src/main/java/com/example/demo/service/ProduitMockServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Produit;
+
+@Service
+public class ProduitMockServiceImpl implements IProduitService {
+
+	List<Produit> produits;
+
+	public ProduitMockServiceImpl() {
+		produits = new ArrayList<Produit>();
+		produits.add(new Produit("Livre", 50, 20));
+		produits.add(new Produit("Cahier", 100, 2.50));
+		produits.add(new Produit("Stylo", 30, 1.20));
+	}
+
+	@Override
+	public List<Produit> getPorduits() {
+		return produits;
+	}
+
+	@Override
+	public void addProduit(Produit produit) {
+		produits.add(produit);
+	}
+
+	@Override
+	public void updateProduit(Produit produit) {
+		produits.remove(produit);
+		produits.add(produit);
+	}
+
+	@Override
+	public void deleteProduit(String ref) {
+		Produit produit = new Produit();
+		produit.setRef(ref);
+		
+		produits.remove(produit);
+	}
+
+}
